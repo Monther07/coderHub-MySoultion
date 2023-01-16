88dc7ffcaa234b825064b22e05b066a66a07738e
MontherG <44753310+Monther07@users.noreply.github.com>
2023-01-17 02:09:21 +0300

    Create lengthOfName.js

---
+++ lengthOfName.js
@@ -0,0 +1,39 @@
+
+function name_char_length(name) {
+    // write your code here
+	return name.length; // not length()
+}
+
+
+
+/*
+وصف التحدي
+قم بكتابة دالة function تستقبل قيمة نصية string. تقوم الدالة بإرجاع عدد الحروف والمسافات في تلك القيمة النصية.
+
+المخرجات المتوقعة
+الاختبار 1
+
+المدخلات (Inputs)
+name = 'Keep smiling'
+المخرجات (Outputs)
+12
+الاختبار 2
+
+المدخلات (Inputs)
+name = 'You can'
+المخرجات (Outputs)
+7
+الاختبار 3
+
+المدخلات (Inputs)
+name = 'awesome'
+المخرجات (Outputs)
+7
+الاختبار 4
+
+المدخلات (Inputs)
+name = 'CoderHub'
+المخرجات (Outputs)
+8
+*/
+
